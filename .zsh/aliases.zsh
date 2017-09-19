e170633389960b3d699c0e50deb1ae12d2a57da7
Adam Labbe <adamlabbe@gmail.com>
2017-09-19 10:42:02 -0400

    Change vivado_enable and disable into functions, and parameterize to allow specifying interface

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,8 +3,6 @@ alias gvim="gvim $* 2>/dev/null"
 alias pyclean="find . | grep -E \"(__pycache__|\.pyc|\.pyo$)\" | xargs rm -rf"
 alias spotify="spotify --force-device-scale-factor=2.0"
 alias acadia_docker="docker run -ti --name acadia --hostname acadia -v /home/adam/projects/idexx:/src -v /home/adam/.ssh:/home/build/.ssh --rm --privileged embedded.idexxi.com:5012/acadia-build bash"
-alias enable_vivado="sudo ip link set dev wifi0 down; sudo ip link set wifi0 address d6:db:e9:70:7b:16"
-alias disable_vivado="sudo ip link set wifi0 address e4:a7:a0:3a:f7:73; sudo ip link set dev wifi0 up"
 alias checkbranch='for dir in *; do if [ -d "$dir/.git" ]; then branch=$(git -C $dir branch | grep \* | sed "s/\* //"); printf "%-25s%-12s\n" "$dir" "$branch"; fi; done'
 alias vi="vim"
 alias grep="grep --color=auto"
@@ -12,11 +10,33 @@ alias zshcolors='for code in {000..255}; do print -P -- "$code: %F{$code}Test%f"
 alias sudo="nocorrect sudo"
 alias dirh="dirs -v"
 
-config() {
+function config() {
     if [[ $@ =~ "^clean" ]]; then
         echo "LOLLLLLLL, you almost blew up your home directory"
     else
         /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+    fi
+}
+
+function enable_vivado() {
+    if [[ "$#" == "0" ]]; then
+        echo "usage: enable_vivado <net_adaptor>"
+    else
+        sudo ip link set dev $1 down
+        sudo ip link set $1 address d6:db:e9:70:7b:16
+        echo "$1" > /tmp/vivado_link
+    fi
+}
+
+function disable_vivado() {
+    if [[ ! -f /tmp/vivado_link ]]; then
+        echo "Vivado network adapter not detected"
+    else
+
+        VIVADO_LINK=$(cat /tmp/vivado_link)
+        sudo ip link set $VIVADO_LINK address e4:a7:a0:3a:f7:73
+        sudo ip link set dev $VIVADO_LINK up
+        rm /tmp/vivado_link
     fi
 }
 
