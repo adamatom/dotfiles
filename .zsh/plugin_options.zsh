3a4863851658066ff3c9b74b19e9ec69c76dff80
Adam Labbe <adamlabbe@gmail.com>
2018-12-20 09:56:49 -0500

    Map shift-enter to execute autosuggestion.

--- .zsh/plugin_options.zsh
+++ .zsh/plugin_options.zsh
@@ -1,1 +1,9 @@
+# Change highlight to use a blue background
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=blue,fg=white,bold'
+
+# Use async suggestion engine that uses zsh/zpty module
+ZSH_AUTOSUGGEST_USE_ASYNC="1"
+
+# Bind F10 to execute the autosuggestion. I map Shift-Return to F10 in terminal
+# emulator.
+bindkey '^[[21~' 'autosuggest-execute'
