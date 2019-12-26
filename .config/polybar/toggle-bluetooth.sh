9a800f4fb58d580a98bcc66e19a35cad0aaf5916
Adam Labbe <adam.labbe@generac.com>
2019-12-26 08:53:23 -0500

    Remove unused files from new environment

--- .config/polybar/toggle-bluetooth.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-bt_blocked=$(rfkill list bluetooth| awk '/Soft blocked: yes/ {print $3}')
-
-if [ "${bt_blocked}" = "" ]; then
-    rfkill block bluetooth
-else
-    rfkill unblock bluetooth
-fi
