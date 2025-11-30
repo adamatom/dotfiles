2fe857e122c00bef079bfd14830045f7fa698f6b
Adam Labbe <adam@hfi.io>
2025-11-30 13:22:18 +0100

    nvim: lspconfig: add command to cleanup the lsp log, `:LspLogClear`

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -22,6 +22,11 @@ return {
       'saghen/blink.cmp',
     },
     config = function()
+      vim.api.nvim_create_user_command("LspLogClear", function()
+        local lsplogpath = vim.fn.stdpath("state") .. "/lsp.log"
+        print(lsplogpath)
+        if io.close(io.open(lsplogpath, "w+b")) == false then vim.notify("Clearning LSP Log failed.", vim.log.levels.WARN) end
+      end, { nargs = 0 })
 
       --  This function gets run when an LSP attaches to a particular buffer.
       --    That is to say, every time a new file is opened that is associated with
